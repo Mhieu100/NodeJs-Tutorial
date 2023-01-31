db97a5333eb2ba5c2f8d9537323e8dfbc6a81657
BlackMan009 <hieunguyen201103@gmail.com>
2023-02-01 00:12:07 +0700

    update

--- index.js
+++ index.js
@@ -146,25 +146,127 @@
 // console.log(qdata.month);
 
 //--------------------------------------------------------------------------
-// Node.js File Server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// Node.js File Server (Summer && Writer)
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
 
+// http.createServer(function (req, res) {
+//     var q = url.parse(req.url, true);
+//     var filename = "." + q.pathname;
+//     fs.readFile(filename, function (err, data) {
+//         if (err) {
+//             res.writeHead(404, { 'Content-Type': 'text/html' });
+//             return res.end("404 Not Found");
+//         }
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
+//--------------------------------------------------------------------------
+// Using a Package
+// var http = require('http');
+// //===================================
+// var uc = require('upper-case');//====
+// //===================================
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(uc.upperCase("Hello World!"));
+//     res.end();
+// }).listen(8080);
+
+//--------------------------------------------------------------------------
+// Events in Node.js
+
+// var fs = require('fs');
+// var rs = fs.createReadStream('./file.txt');
+// rs.on('open', function () {
+//     console.log('The file is open');
+// });
+
+//--------------------------------------------------------------------------
+// Events in Node.js
+
+// var events = require('events');
+// var eventEmitter = new events.EventEmitter();
+
+// //Create an event handler:
+// var myEventHandler = function () {
+//     console.log('I hear a scream!');
+// }
+
+// //Assign the eventhandler to an event:
+// eventEmitter.on('scream', myEventHandler);
+
+// //Fire the 'scream' event:
+// eventEmitter.emit('scream');
+
+//--------------------------------------------------------------------------
+// Upload Files
+
+// var http = require('http');
+// var formidable = require('formidable');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//     if (req.url == '/fileupload') {
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function (err, fields, files) {
+//             var oldpath = files.filetoupload.filepath;
+//             var newpath = 'C:/Users/admin/' + files.filetoupload.originalFilename;
+//             fs.rename(oldpath, newpath, function (err) {
+//                 if (err) throw err;
+//                 res.write('File uploaded and moved!');
+//                 res.end();
+//             });
+//         });
+//     } else {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+//         res.write('<input type="file" name="filetoupload"><br>');
+//         res.write('<input type="submit">');
+//         res.write('</form>');
+//         return res.end();
+//     }
+// }).listen(8080);
+
+//--------------------------------------------------------------------------
+// The Nodemailer Module
+
+
+// var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: "tktestsendmail1@gmail.com",
+//         pass: "fldaajtjaqxcvoxk"
+//     }
+// });
+
+// var mailOptions = {
+//     from: "tktestsendmail1@gmail.com",
+//     to: "tktestsendmail2@gmail.com",
+//     subject: 'Sending Email using Node.js {- Hello World -}',
+//     text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
 //--------------------------------------------------------------------------
